7f56c3a2ca67fdfab52c61b0c71b6f0bf1b6a409
gitCodeCommitUser <alibsheikhli@gmail.com>
2019-11-19 11:54:03 +0300

    Updates

---
+++ precache-manifest.868a108bd3afb42c0521569f0bb85703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e22d60db243ab4dc8e02fa870c2ea58",
+    "url": "/alisheikhli/reactApp/index.html"
+  },
+  {
+    "revision": "cc96f49a1f8af299c891",
+    "url": "/alisheikhli/reactApp/static/css/main.11a5a24b.chunk.css"
+  },
+  {
+    "revision": "835d023676b0ae22c7c7",
+    "url": "/alisheikhli/reactApp/static/js/2.174eeef5.chunk.js"
+  },
+  {
+    "revision": "cc96f49a1f8af299c891",
+    "url": "/alisheikhli/reactApp/static/js/main.b57cda35.chunk.js"
+  },
+  {
+    "revision": "3e39eb82654fff7b8cf1",
+    "url": "/alisheikhli/reactApp/static/js/runtime-main.a5b77941.js"
+  }
+]);
